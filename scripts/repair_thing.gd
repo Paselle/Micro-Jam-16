7ebcf97335ef4ad164c2a328998c57044a61dd8a
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2024-06-16 15:48:23 +1200

    noide things

--- scripts/repair_thing.gd
+++ scripts/repair_thing.gd
@@ -2,14 +2,15 @@ class_name RepairThing extends Area3D
 
 
 const NOISE_TEXTURE_2D = preload("res://resources/noise_texture_2d.tres")
+const FAST_NOISE_LITE = preload("res://resources/fast_noise_lite.tres")
 
 @onready var mesh = $Mesh
 
 
 func _ready():
-	var new_noise_texture = NOISE_TEXTURE_2D
-	new_noise_texture.noise.seed = randi_range(0, 10000)
-	mesh.mesh.material.set_shader_parameter("NoiseTexture", new_noise_texture)
+	var new_noise_texture = FAST_NOISE_LITE.duplicate()
+	new_noise_texture.seed = randi_range(0, 10000)
+	mesh.mesh.material.get_shader_parameter("NoiseTexture").noise = new_noise_texture
 
 
 func repair() -> void:
