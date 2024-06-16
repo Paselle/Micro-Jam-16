24ff5aa39ce12c53d87e9f62a3b87ed0a1005ef6
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2024-06-16 15:32:03 +1200

    repair things

--- scripts/singleton.gd
+++ scripts/singleton.gd
@@ -5,6 +5,7 @@ extends Node
 var shipping := true
 var ship_display: SubViewportContainer
 var switch_cooldown: Timer
+var resources := 10
 
 
 func switch_shipping() -> void:
@@ -15,3 +16,7 @@ func switch_shipping() -> void:
 		else:
 			ship_display.modulate = Color.TRANSPARENT
 		switch_cooldown.start()
+
+
+func decrease_resources() -> void:
+	resources -= 1
