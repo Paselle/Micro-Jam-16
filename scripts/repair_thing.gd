0a1c3549d12a3762eb02fb13537727746fb9d16a
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2024-06-16 20:27:57 +1200

    noise fix

--- scripts/repair_thing.gd
+++ scripts/repair_thing.gd
@@ -1,4 +1,3 @@
-@tool 
 class_name RepairThing extends Area3D
 
 
@@ -9,9 +8,10 @@ const FAST_NOISE_LITE = preload("res://resources/fast_noise_lite.tres")
 
 
 func _ready():
+	mesh.mesh = mesh.mesh.duplicate(true)
 	var new_noise_texture = NOISE_TEXTURE_2D.duplicate()
 	var new_noise = FAST_NOISE_LITE.duplicate()
-	new_noise_texture.seed = randi_range(0, 10000)
+	new_noise.seed = randi_range(0, 10000)
 	new_noise_texture.noise = new_noise
 	mesh.mesh.material.set_shader_parameter("NoiseTexture", new_noise_texture)
 
